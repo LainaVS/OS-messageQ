8127473b872a950970cec22574aa6eb1298cd053
elaina rohlfing <rohlfing@opsys.cs.umsl.edu>
2023-10-14 15:23:11 -0500

    oss initializes clock, execs worker, worker attaches and calculates termination time, then prints --starting info, tons of commented out code

--- oss.c
+++ oss.c
@@ -5,27 +5,20 @@
 //  Clock
 //---------------------------------------------------
 
-
 #include <stdlib.h>
-//#include <ctype.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include "pcb.h"
 #include "validate.h"
 #include "macros.h" //system clock keys - might rename file
-/*
-  //initialize the system clock (alternately use #define)
-  const int sysClock_skey = ftok("oss.c", 1);   //generate seconds key based on file
-  const int sysClock_nskey = ftok("oss.c", 2);  //generate nanoseconds key based on file  
-*/
 
-static void incrementClock();
-static void parent();
-static void child();
+//static void incrementClock(); //not yet
+//static void parent(); //probably toss
+//static void child();  //probably toss
 static void help();
 
-static struct PCB processTable[20];
+//static struct PCB processTable[20]; //not yet
 
 
 int main(int argc, char** argv) {
@@ -55,20 +48,70 @@ int main(int argc, char** argv) {
       return 1;
     }
   }
-  
+    
   //allocate memory to shared keys
-  int shmid_seconds = shmget(SYSCLK_SKEY, BUFF_SZ, IPC_CREAT | 0666);
-  int shmid_nanoseconds = shmget(SYSCLK_NSKEY, BUFF_SZ, IPC_CREAT | 0666);
+  int pshmid_seconds = shmget(SYSCLK_SKEY, BUFF_SZ, IPC_CREAT | 0666);
+  int pshmid_nanoseconds = shmget(SYSCLK_NSKEY, BUFF_SZ, IPC_CREAT | 0666);
 
-  if(shmid_seconds <= 0 || shmid_nanoseconds <= 0)
+  if(pshmid_seconds <= 0 || pshmid_nanoseconds <= 0)
     fatal("Parent failed to create sys clock in shared memory");
   
-  //Attach to shared memory (in parent and in child)
-  int * sysClock_seconds = shmat(shmid_seconds, 0, 0);
-  int * sysClock_nanoseconds = shmat(shmid_nanoseconds, 0, 0);
+  //Attach to shared memory
+  int * psysClock_seconds = shmat(pshmid_seconds, 0, 0);
+  int * psysClock_nanoseconds = shmat(pshmid_nanoseconds, 0, 0);
   
-  if (sysClock_seconds <= 0 || sysClock_nanoseconds <= 0) 
+  if (psysClock_seconds <= 0 || psysClock_nanoseconds <= 0) 
     fatal("Parent failed to attach to Sysclock in shared memory");
+    
+  //initialize system clock to zero
+  *psysClock_seconds = 0;
+  *psysClock_nanoseconds = 0;
+
+  /****************************************************
+   1: create fake args for first worker test
+   2: then process oss input (s and random ns)
+   **max int: 2147483647
+   **ns to s: 1,000,000,000
+   ****************************************************/
+   char * s_arg = "3";
+   char * ns_arg = "330300";
+   //char* args[] = {"./worker", "3", "330330", NULL};
+   /****************************************************/    
+
+/****************************************************
+ for testing with worker: fork and exec single worker 
+ ****************************************************/
+  pid_t workerPid = fork();
+  
+  if (workerPid == 0) {  // child process to launch one worker
+    char* args[] = {"./worker", s_arg, ns_arg, NULL};
+    
+    // terminate if exec call fails
+    if (execvp(args[0], args) == -1) {
+      perror("execvp");
+
+      fatal("exec call failed");
+    }
+    ;
+  } else {
+   /*********************************
+    test without incrementing 1st
+    *********************************
+   int s, ns;
+    for(s = 0; s < 60; s++) {
+      //increment System clock
+      *psysClock_seconds++;
+      
+      for(ns = 0; ns < 100000; ns + 1000) {
+        //increment System Clock
+        *psysClock_seconds += 1000;
+        
+      }
+    }
+    *********************************/
+    wait(NULL); //change to non blocking wait call
+  }
+ /****************************************************/
   
 /***************************************************
  * 2) go into 60 second loop ->
@@ -84,18 +127,20 @@ int main(int argc, char** argv) {
   ***************************************************/
   
   /****************************************************
+   From psuedocode: loop to fork and exec call to workers. 
+   needs work!!!
    ****************************************************
-   * From psuedocode: loop to fork and exec call to workers. 
-   ****************************************************/
   bool stillChildrenToLaunch = true;
   bool childHasTerminated = true;
   while(stillChildrenToLaunch) {
     // incrementClock();
     sleep(1);
-    *sysClock_seconds += 1;
-    *sysClock_nanoseconds += 1000;
+    *psysClock_seconds += 1;
+    *psysClock_nanoseconds += 1000;
     //print every half second (simulated clock time):
-    printf("Parent: Incrementing SysClock at %ds %dns\n", *sysClock_seconds, *sysClock_nanoseconds);
+
+
+    printf("Parent: Incrementing SysClock at %ds %dns\n", *psysClock_seconds, *psysClock_nanoseconds);
     printf("process table"); //remove
      
     if(childHasTerminated) {
@@ -107,26 +152,12 @@ int main(int argc, char** argv) {
       stillChildrenToLaunch = false;
     }
   }
-  
-  /***************************************************
-  ***************************************************/
-  
-  
-  
-  /****************************************
-  //convert iter variable to char * for processing 
-  //(source https://www.geeksforgeeks.org/sprintf-in-c/)
-  char iterChar[50];
-  sprintf(iterChar, "%d", iter); 
-  ****************************************/
 
+  ***************************************************/
   
   return EXIT_SUCCESS;
 }
 
-static void incrementClock() {
-
-}
 
 /**********************************************************
  * parent desc (oss tasks)
@@ -149,7 +180,7 @@ static void incrementClock() {
  *       6) when child term: update childPCB entry
  *          7) if proc (worker) remaining: loop
  *          8) else: fin
- *********************************************************/
+ *********************************************************
 static void parent() {
   int i;
   
@@ -185,7 +216,7 @@ static void parent() {
   
   printf("Parent Terminating\n");
 }
-
+ *********************************************************/
 /**********************************************************
  * Child will be worker - exec'd in parent, (need to refactor)
  * worker attaches to shared memory
@@ -203,7 +234,7 @@ static void parent() {
  *     if terminationTarget reached 
  *        >> output currTime, terminationTarget, 'terminating'
  *        terminate
- *********************************************************/
+ *********************************************************
 static void child() {
   int i;
   
@@ -234,6 +265,7 @@ static void child() {
   shmdt(sysClock_seconds);
   shmdt(sysClock_nanoseconds);
 }
+**********************************************************************/
 
 static void help() {
   printf("\n\n\tinfo.\n");
--- worker.c
+++ worker.c
@@ -38,6 +38,9 @@ int main(int argc, char** argv) {
     help();
   }
   
+  char * msg; //declare message variable for worker output statements
+  //int secondsPassed = 0; //counter to keep track of worker progress //used in loop
+  
   //set time that worker should be allowed to stay in system
   int sec = arraytoint(argv[1]);
   int nano = arraytoint(argv[2]);
@@ -53,20 +56,52 @@ int main(int argc, char** argv) {
   int * sysClock_seconds = shmat(shmid_seconds, 0, 0);
   int * sysClock_nanoseconds = shmat(shmid_nanoseconds, 0, 0);
   
-  //read (and store) current time
-  printf("current time: %ds %dns", *sysClock_seconds, *sysClock_nanoseconds);
-  
+  //initialize system clock to zero 
+  /*(((for testing worker only - will be done in parent)))*********
+  *sysClock_seconds = 0;
+  *sysClock_nanoseconds = 0;*/
+   
+  //store (?start and?) current time
+  int curr_sec = *sysClock_seconds;
+  int curr_nano = *sysClock_nanoseconds;
+  //int start_sec = curr_sec;    //used in loop
+  //int start_nano = curr_nano; //might not need //used to start loop  
+ 
   //calculate terminationTime (future)
-  printf("terminate at %ds %dns", *sysClock_seconds + sec, *sysClock_nanoseconds + nano);
-  
+  int term_sec = curr_sec + sec;
+  int term_nano = curr_nano + nano;
+  /***********************************************
+   Start here for testing - test to see if worker is attached
+  ***********************************************/
+  //output a status update at start-up
+  msg = "--Just Starting";
+  printf("\n\tWORKER PID:%d  PPID:%d  SysClockS:%d  SysClockNano:%d  TermTimeS:%d  TermTimeNano:%d \n\t%s\n\n", getpid(), getppid(), *sysClock_seconds, *sysClock_nanoseconds, term_sec, term_nano, msg);
+    
   
   /************************************************
-  loop from P1
+  loop from P1 - test above first
   *************************************************
-  for (int i = 0; i < sec; i++) { // not correct, just a placeholder so I can eat lunch.
-    printf("\n\tWORKER PID:%d  PPID:%d  Iteration:%d  before sleeping\n", getpid(), getppid(), i);
-    sleep(1);
-    printf("\n\tWORKER PID:%d  PPID:%d  Iteration:%d  after sleeping\n", getpid(), getppid(), i);
+  for (int i = start_nano; i < term_nano; i+1000) { //not correct, just a placeholder so I can do something else
+    //if at least 1 second has passed, output status update.
+    if (curr_sec < *sysClock_seconds) {
+      //update current time vars
+      curr_sec = *sysClock_seconds;
+      curr_nano = *sysClock_nanoseconds;
+      
+      //either increment secondsPassed or subtract curr_sec - start_sec
+      secondsPassed = curr_sec - start_sec;
+      
+      //print output
+      msg = "msgDependent-on-prog";
+      printf("\n\tWORKER PID:%d  PPID:%d  SysClockS:%d  SysClockNano:%d  TermTimeS:%d  TermTimeNano:%d\n", getpid(), getppid(), *sysClock_seconds, *sysClock_nanoseconds, term_sec, term_nano);
+      printf("--%d seconds have passed since starting", secondsPassed);
+    }
+    if (*sysClock_seconds >= term_sec && *sysClock_nanoseconds >= term_nano) {
+      msg = "--Terminating";
+      printf("\n\tWORKER PID:%d  PPID:%d  SysClockS:%d  SysClockNano:%d  TermTimeS:%d  TermTimeNano:%d \n%s\n", getpid(), getppid(), *sysClock_seconds, *sysClock_nanoseconds, term_sec, term_nano, msg);
+    }
+    
+    sleep(1);  //to be removed
   }
   printf("\n\n");
   ************************************************/
